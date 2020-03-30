f4b4a8b83de8f7d37553cca4d6743f3cb5cb9d03
Nikita <32339836+nacenik@users.noreply.github.com>
2020-03-30 21:45:22 +0300

    Adding base entity . Closes #10

--- pet-clinic-data/src/main/java/net/oleksin/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/net/oleksin/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package net.oleksin.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/net/oleksin/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/net/oleksin/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package net.oleksin.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
