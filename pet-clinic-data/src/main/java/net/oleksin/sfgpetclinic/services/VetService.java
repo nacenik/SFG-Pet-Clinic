aa4a36d6b0c4307c0a51d7406ccb48ddee9108e9
Nikita <32339836+nacenik@users.noreply.github.com>
2020-03-31 20:04:27 +0300

    refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/net/oleksin/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/net/oleksin/sfgpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package net.oleksin.sfgpetclinic.services;
 
 import net.oleksin.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save (Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
