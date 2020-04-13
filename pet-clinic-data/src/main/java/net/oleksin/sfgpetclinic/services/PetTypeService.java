6dd484c3ce8809f6ee902149958e023a61237a13
Nikita <32339836+nacenik@users.noreply.github.com>
2020-04-13 19:52:35 +0300

    Created and added PetTypeService to project. Closes #29

---
+++ pet-clinic-data/src/main/java/net/oleksin/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package net.oleksin.sfgpetclinic.services;
+
+import net.oleksin.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/net/oleksin/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package net.oleksin.sfgpetclinic.services.map;
+
+import net.oleksin.sfgpetclinic.model.PetType;
+import net.oleksin.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService{
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
