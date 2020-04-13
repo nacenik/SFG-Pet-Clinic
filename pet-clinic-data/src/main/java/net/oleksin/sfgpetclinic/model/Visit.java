72ba59a904e5d1e7fc85049d6a6ae162d2d3d010
Nikita <32339836+nacenik@users.noreply.github.com>
2020-04-13 19:29:45 +0300

    Added Visits and update owner with pet relation. Closes #26

---
+++ pet-clinic-data/src/main/java/net/oleksin/sfgpetclinic/model/Visit.java
@@ -0,0 +1,33 @@
+package net.oleksin.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends Person {
+    private LocalDate birthDate;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
