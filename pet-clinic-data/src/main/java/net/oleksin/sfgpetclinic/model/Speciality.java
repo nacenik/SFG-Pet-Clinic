f3b83e391d21eccdbb01a2ea991e089b89e46049
Nikita <32339836+nacenik@users.noreply.github.com>
2020-04-13 19:37:13 +0300

    Added specialities, and added to vets. Closes #27

---
+++ pet-clinic-data/src/main/java/net/oleksin/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package net.oleksin.sfgpetclinic.model;
+
+public class Speciality {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
