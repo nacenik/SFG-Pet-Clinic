3ad3399a34ddcf81397781bb905d037bbd6e745e
Nikita <32339836+nacenik@users.noreply.github.com>
2020-05-01 22:36:47 +0300

    Upgraded to Junit 5

---
+++ pet-clinic-web/src/test/java/net/oleksin/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -0,0 +1,19 @@
+package net.oleksin.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SfgPetClinicApplicationTest {
+
+    @Test
+    void contentLoads(){
+
+    }
+
+}
