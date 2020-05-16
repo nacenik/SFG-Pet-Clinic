4d4b0c7ad3395e7c1e7a0da9a0abadfc7c77ed6b
Nikita <32339836+nacenik@users.noreply.github.com>
2020-05-16 11:55:05 +0300

    Display Owner Record. Closes #53

--- pet-clinic-web/src/main/java/net/oleksin/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/net/oleksin/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package net.oleksin.sfgpetclinic.controllers;
 import net.oleksin.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -15,7 +18,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String listOfPets(Model model){
 
         model.addAttribute("owners", ownerService.findAll());
@@ -23,9 +26,17 @@ public class OwnerController {
         return "owners/index";
     }
 
-    @RequestMapping("/find")
+    @GetMapping("/find")
     public String findOwners(){
 
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+        modelAndView.addObject(this.ownerService.findById(ownerId));
+
+        return modelAndView;
     }
 }
